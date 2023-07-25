0c5feffb4409ee75bc341809716ee9ba07dc4a21
shyam640 <shyamvashishtha640@gmail.com>
2023-07-25 12:47:58 +0530

    updated

--- frontend/src/components/molecules/Testimonial/index.js
+++ frontend/src/components/molecules/Testimonial/index.js
@@ -20,7 +20,7 @@ const Testimonial = () => {
   const animation = useAnimation();
 
   const fetchTestimonial = async () => {
-    const {data} = await fetch('/api/testimonials');
+    const {data} = await axios.get('/api/testimonials');
     console.log(data);
     setTestimonial(data);
   }
