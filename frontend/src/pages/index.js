9cd06027d88f03923c6cb7e79e28b8ac52cc7b29
shyam640 <shyamvashishtha640@gmail.com>
2023-07-30 20:33:45 +0530

    Production Ready

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -3,5 +3,8 @@ import Blog from "./Blog";
 import Contact from "./Contact";
 import Home from "./Home";
 import Project from "./Project";
+import CodingProfile from "./CodingProfile";
+import Auth from "./Auth";
+import Upload from "./Upload";
 
-export { About, Blog, Contact, Home, Project};
+export { About, Blog, Contact, Home, Project, CodingProfile, Auth, Upload};
